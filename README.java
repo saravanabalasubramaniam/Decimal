6a37bcb7ba0d175ddf3c8c1947068d8bb3cc3e1f
saravana <saravanasaru14313@gmail.com>
2019-02-20 13:43:00 +0530

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class Decimal
+{
+  public static void main(String args[])
+  {
+	Scanner input=new Scanner(System.in);
+	float a=input.nextFloat();
+	System.out.println(Math.ceil(a));
+   }
+}
